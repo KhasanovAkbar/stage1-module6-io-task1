50c8ad6d36479c5510fe955c1337cbf48d13aff5
KhasanovAkbar <akbar.hasanov0630@gmail.om>
2023-07-20 16:09:29 +0500

    first commit

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,50 @@
 package com.epam.mjc.io;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        try (FileInputStream fileInputStream = new FileInputStream(file)) {
+            int ch;
+            while ((ch = fileInputStream.read()) != -1) {
+                stringBuilder.append((char) ch);
+            }
+
+        } catch (FileNotFoundException exception) {
+            exception.printStackTrace();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Profile profile = new Profile();
+
+        String string = stringBuilder.toString();
+        String[] split = string.split("\n");
+
+        for (String s : split) {
+            String[] split1 = s.split(" ");
+            switch (split1[0]) {
+                case "Name:":
+                    profile.setName(split1[1]);
+                    break;
+                case "Age:":
+                    profile.setAge(Integer.parseInt(split1[1]));
+                    break;
+                case "Email:":
+                    profile.setEmail(split1[1]);
+                    break;
+                case "Phone:":
+                    profile.setPhone(Long.parseLong(split1[1]));
+                    break;
+            }
+        }
+        return profile;
     }
 }
---
+++ src/main/java/com/epam/mjc/io/Main.java
@@ -0,0 +1,13 @@
+package com.epam.mjc.io;
+
+import java.io.File;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File file = new File("src/main/resources/Profile.txt");
+        FileReader fileReader = new FileReader();
+        fileReader.getDataFromFile(file);
+    }
+}
